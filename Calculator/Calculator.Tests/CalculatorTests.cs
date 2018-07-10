0831061090303a1b619466a97726cd07e28254cf
Monique Govender <MoniqueG@SAHOMELOANS.COM>
2018-07-10 10:24:39 +0200

    two tests

---
+++ Calculator/Calculator.Tests/CalculatorTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorKata.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase(null)]
+        public void Calculate_GivenAnEmptyString_ShouldReturn0(string input)
+        {
+            //Arrange
+            var expected = 0;
+            Calculator sut = new Calculator();
+
+            //Act
+            var actual = sut.Calculate(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Calculate_GivenTwoNumbersAndAdditionOperation_ShouldReturnTheSum()
+        {
+            //Arrange
+            var input = "1 + 1";
+            var expected = 2;
+            Calculator sut = new Calculator();
+
+            //Act
+            var actual = sut.Calculate(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Calculator/Calculator/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorKata
+{
+    public class Calculator
+    {
+        public int Calculate(string input)
+        {
+            if(string.IsNullOrWhiteSpace(input))
+            {
+                return 0;
+            }
+            return 2;
+        }
+    }
+}
